549c2b46d049d699dd65a0b1f1f425b0b38b4379
Ruchit2993 <ruchitpitaliya2003@gmail.com>
2025-10-23 22:11:44 +0530

    WIP: sidebar

--- AngApp/src/app/app.routes.ts
+++ AngApp/src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { Login } from './froms/login/login';
 import { Register } from './froms/register/register';
 import { Users } from './users/users';
 import { AddUser } from './users/add-user/add-user';
+import { Contact } from './contact/contact';
+import { Home } from './home/home';
+import { About } from './about/about';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -10,5 +13,8 @@ export const routes: Routes = [
   { path: 'register', component: Register },
   { path: 'users', component: Users },
   { path: 'add-user', component: AddUser },
-  { path: '**', redirectTo: 'login' }
+  // { path: '**', redirectTo: 'login' },
+  { path: 'contact', component: Contact },
+  { path: 'about', component: About },
+  { path: 'home', component: Home }
 ];
